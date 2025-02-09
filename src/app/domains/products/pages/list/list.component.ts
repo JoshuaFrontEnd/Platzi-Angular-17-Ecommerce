1dc47aa6a81631da9db1e95c2ab2a07a32406f04
Joshua <hola@joshua.cl>
2025-02-09 20:08:52 -0300

    Creando el Header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.models';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -33,6 +34,27 @@ export class ListComponent {
         title: 'Product 3',
         price: 300,
         image: 'https://picsum.photos/600?r=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 4',
+        price: 400,
+        image: 'https://picsum.photos/600?r=24',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 5',
+        price: 500,
+        image: 'https://picsum.photos/600?r=25',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 6',
+        price: 600,
+        image: 'https://picsum.photos/600?r=26',
         creationAt: new Date().toISOString(),
       },
     ];
