f57598684d49e2ff65b32ee078b3991dd1611484
Joshua <hola@joshua.cl>
2025-02-09 18:24:18 -0300

    Ciclo de vida de componentes: ngOnInit

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -10,16 +10,36 @@ export class CounterComponent {
   @Input({ required: true }) duration = 0;
   @Input({ required: true }) message = '';
 
-  // Esto se ejecuta antes de que se renderice el componente, no colocar nada async
+  // Esto se ejecuta una sola vez, antes de que se renderice el componente, no colocar nada async
   constructor() {
     console.log('constructor');
     console.log('-'.repeat(10));
   }
 
-  // Este evento se ejecuta antes y durante el render del componente y permite identificar que cambió
+  // Este evento se ejecuta antes y durante el render del componente, cuando exista algun cambio y permite identificar que cambió
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges');
     console.log(changes);
+    console.log('-'.repeat(10));
+  }
+
+  // Este evento se ejecuta una sola vez, despues de que se renderice el componente, acá recien se pueden hacer peticiones asíncronas, async, then, suscribe, etc
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    console.log('-'.repeat(10));
+  }
+
+  // Este evento se ejecuta una sola vez inmeditamente despues de ngOnInit y sirve para detectar si se renderizaron los hijos del componente
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  // Este evento se ejecuta inmediatamente antes de que se destruya el componente
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
     console.log('-'.repeat(10));
   }
 }
