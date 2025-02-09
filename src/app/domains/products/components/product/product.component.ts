8de6c187cc4cf7d024737f7195389bb12557c1ae
Joshua <hola@joshua.cl>
2025-02-09 20:00:33 -0300

    Creando componente de productos

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.models';
 
 @Component({
   selector: 'app-product',
@@ -8,14 +9,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
   // img = 'https://picsum.photos/300?r=' + Math.random();
-  @Input({ required: true }) img: string = '';
-  @Input({ required: true }) price: number = 0;
-  @Input({ required: true }) title: string = '';
+
+  // Declarando cada atributo del objeto
+  // @Input({ required: true }) img: string = '';
+  // @Input({ required: true }) price: number = 0;
+  // @Input({ required: true }) title: string = '';
+
+  // Declarando solo el objeto con todos sus atributos
+  @Input({ required: true }) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
     console.log('Click from child');
-    this.addToCart.emit('Este es un mensaje desde el hijo:' + this.title);
+    this.addToCart.emit(
+      'Este es un mensaje desde el hijo:' + this.product.title
+    );
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/600?r=21',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 200,
+        image: 'https://picsum.photos/600?r=22',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 300,
+        image: 'https://picsum.photos/600?r=23',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
+
   // Evento recibido desde el hijo
   fromChild(event: string) {
     console.log('Mensaje recibido desde el hijo:', event);
