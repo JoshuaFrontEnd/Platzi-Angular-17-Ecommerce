eff46528b2699eaa917ac817cc02e4b0dcf3a410
Joshua <hola@joshua.cl>
2025-03-04 16:45:49 -0300

    Galeria de imagenes

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input, signal } from '@angular/core';
+import { Product } from '@shared/models/product.models';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent {
+  @Input() id?: string;
+  product = signal<Product | null>(null);
+  cover = signal('');
+  private productService = inject(ProductService);
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getOne(this.id).subscribe({
+        next: (product) => {
+          this.product.set(product);
+          if (product.images.length > 0) {
+            this.cover.set(product.images[0]);
+          }
+        },
+      });
+    }
+  }
+
+  changeCover(newImg: string) {
+    this.cover.set(newImg);
+  }
 }
